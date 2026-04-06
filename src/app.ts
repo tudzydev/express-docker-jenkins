c8b873aa4b8ffb6cf0b7690d3a4f7067aaf54786
tudzydev <674259009@webmail.npru.ac.th>
2026-04-06 17:47:34 +0700

    make users root

--- src/app.ts
+++ src/app.ts
@@ -26,5 +26,14 @@ app.get('/api/health', (_: Request, res: Response) => {
   })
 })
 
+app.get('/api/users', (_: Request, res: Response) => {
+  const users = [
+    { id: 1, name: 'Johne doe' },
+    { id: 1, name: 'Johne doe' }
+  ]
+  res.json(
+    users
+  )
+})
 // Start server
 app.listen(port, () => console.log(`Application is running on port ${port}`))
