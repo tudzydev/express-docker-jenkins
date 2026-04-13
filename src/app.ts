bfae36fa35fdee23e014eb55649031e7a5f84df6
tudzydev <674259009@webmail.npru.ac.th>
2026-04-13 15:44:26 +0700

    .

--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,7 @@ app.get('/api/orders', (_: Request, res: Response) => {
     { id: 2, userId: 2, productId: 3, quantity: 2 },
     { id: 3, userId: 1, productId: 1, quantity: 1 },
     { id: 4, userId: 2, productId: 4, quantity: 1 },
-    { id: 5, userId: 3, produtId: 5, quantity: 1 }
+    { id: 5, userId: 3, produtId: 5, quantity: 1 },
   ]
   res.json(orders)
 })
